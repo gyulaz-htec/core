c0504493badd2f3fbc55319b1c8a1b5bfbc69f63
Gyula Zakor <gyula.zakor@htecgroup.com>
2024-06-03 11:27:41 +0000

    Hipify with CMake

---
+++ src/triton_rocm_hipify.cmake
@@ -0,0 +1,40 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+find_package(Python3 COMPONENTS Interpreter REQUIRED)
+
+function(auto_set_source_files_hip_language)
+  foreach(f ${ARGN})
+    if(f MATCHES ".*\\.cu$")
+      set_source_files_properties(${f} PROPERTIES LANGUAGE HIP)
+    endif()
+  endforeach()
+endfunction()
+
+# cuda_dir must be relative to REPO_ROOT
+function(hipify src_files out_generated_files)
+  set(hipify_tool ${REPO_ROOT}/src/amd_hipify.py)
+
+  message(STATUS "@@@ File list ${src_files}")
+
+  foreach(f ${src_files})
+    message("@@@ Processing ${f}")
+    file(RELATIVE_PATH cuda_f_rel "${REPO_ROOT}" ${f})
+    string(REPLACE "cuda" "rocm" rocm_f_rel ${cuda_f_rel})
+    set(f_out "${CMAKE_CURRENT_BINARY_DIR}/amdgpu/${rocm_f_rel}")
+    add_custom_command(
+      OUTPUT ${f_out}
+      COMMAND Python3::Interpreter ${hipify_tool}
+      --hipify_perl ${TRITON_HIPIFY_PERL}
+      ${f} -o ${f_out}
+      DEPENDS ${hipify_tool} ${f}
+      )
+    message(STATUS "@@@ Hipify: ${cuda_f_rel} -> amdgpu/${rocm_f_rel}")
+    list(APPEND generated_files ${f_out})
+
+  endforeach()
+
+  set_source_files_properties(${generated_files} PROPERTIES GENERATED TRUE)
+  auto_set_source_files_hip_language(${generated_files})
+  set(${out_generated_files} ${generated_files} PARENT_SCOPE)
+endfunction()
