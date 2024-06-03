93df751a1533cea2f7ec619755b0d89dc5890464
Gyula Zakor <gyula.zakor@htecgroup.com>
2024-06-03 12:57:53 +0000

    Debug

--- src/triton_rocm_hipify.cmake
+++ src/triton_rocm_hipify.cmake
@@ -16,10 +16,11 @@ function(hipify src_files out_generated_files)
   set(hipify_tool ${REPO_ROOT}/src/amd_hipify.py)
 
   message(STATUS "@@@ File list ${src_files}")
+  message(STATUS "@@@ out_generated_files ${out_generated_files}")
 
   foreach(f ${src_files})
-  set(cuda_f_rel "${REPO_ROOT}/src/${f}")
-  message("@@@ Processing ${cuda_f_rel}")
+    set(cuda_f_rel "${REPO_ROOT}/src/${f}")
+    message("@@@ Processing ${cuda_f_rel}")
     string(REPLACE "cuda" "rocm" rocm_f_rel ${cuda_f_rel})
     set(f_out "${CMAKE_CURRENT_BINARY_DIR}/amdgpu/${rocm_f_rel}")
     add_custom_command(
@@ -31,7 +32,6 @@ function(hipify src_files out_generated_files)
       )
     message(STATUS "@@@ Hipify: ${cuda_f_rel} -> amdgpu/${rocm_f_rel}")
     list(APPEND generated_files ${f_out})
-
   endforeach()
 
   set_source_files_properties(${generated_files} PROPERTIES GENERATED TRUE)
